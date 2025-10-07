e70cb340d28424e2165c3b0600f2d4c1d037e718
Sahil Yatam <sahilyatam9@gmail.com>
2025-10-07 16:26:32 +0530

    feat(middleware): add global error handler middleware

---
+++ backend/shared/middlewares/globalErrorHandler.js
@@ -0,0 +1,26 @@
+import logger from "../utils/monitoring/logger.js";
+
+export const errorHandler = (err, req, res, next) => {
+    if(res.headersSent){
+        return next(err);
+    }
+
+    logger.error(err.message, {
+        stack: err.stack
+    });
+
+    res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Internal Server Error"
+    });
+};
+
+// Catches 404 routes
+export const notFoundHandler = (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: `Route ${req.originalUrl} not found`,
+        errors: [],
+        data: null,
+    })
+}
