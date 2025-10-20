c40cdfcfa07ab67b801ffc03935f0618540b8e3d
Sahil Yatam <sahilyatam9@gmail.com>
2025-10-20 15:59:34 +0530

    feat(utils/redis): add Redis utilities for caching and data management

---
+++ backend/src/utils/redis/redis.util.js
@@ -0,0 +1,34 @@
+import {redis} from "../../config/redis.config.js"
+
+const buildTokenKey = (userId, hashedToken) => {
+    return `token:${userId}:${hashedToken}`;
+}
+
+const cacheToken = async (userId, hashedToken, expiresAt) => {
+    const ttlSeconds = Math.max(1, Math.floor((new Date(expiresAt) - Date.now()) / 1000 ));
+
+    const key = buildTokenKey(userId, hashedToken);
+    await redis.set(key, "1", "EX", ttlSeconds);
+
+    return key;
+}
+
+const removeToken = async(userId, hashedToken) => {
+    const key = buildTokenKey(userId, hashedToken);
+    await redis.del(key);
+}
+
+const findTokenKey = async(hashedToken) => {
+    const stream = redis.scanStream({match: `token:*:${hashedToken}`, count: 100});
+    for await (const keys of stream){
+        if(keys.length > 0) return keys[0];
+    }
+    return null;
+}
+
+export const redisUtils = {
+    buildTokenKey,
+    cacheToken,
+    removeToken,
+    findTokenKey
+}
