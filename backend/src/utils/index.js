472fe1f23508a848cd6b95c29977cfe1b297328b
Sahil Yatam <sahilyatam9@gmail.com>
2025-10-20 16:00:00 +0530

    chore(utils): add index.js to consolidate utility exports

---
+++ backend/src/utils/index.js
@@ -0,0 +1,21 @@
+export {default as logger} from "./monitoring/logger.js"
+
+export * from "./handlers/asyncHandler.js"
+
+export * from "./response/ApiError.js"
+export * from "./response/ApiResponse.js"
+
+export * from "./crypto/password.utils.js"
+export * from "./crypto/encryptToken.js"
+export * from "./crypto/otp.utils.js"
+
+export * from "./jwt/generateAccAndRefrToken.js"
+export * from "./jwt/cookies.js"
+
+export * from "./email-template/otp.template.js"
+export * from "./email-template/welcome.template.js"
+export * from "./email-template/resetPassword.template.js"
+
+export * from "./redis/redis.util.js"
+
+export * from "./message/message.utils.js";
