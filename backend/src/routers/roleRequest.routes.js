bf6951eb95d0a6878680a3431ae7d65d4e40d52f
Sahil Yatam <sahilyatam9@gmail.com>
2025-10-30 16:16:14 +0530

    feat(routes): add roleRequest routes

---
+++ backend/src/routers/roleRequest.routes.js
@@ -0,0 +1,51 @@
+import { Router } from "express";
+import { roleRequestController } from "../controllers/roleRequest.controller.js";
+
+import { validateRequest } from "../middlewares/joiValidation.middleware.js";
+
+import { authentication } from "../middlewares/auth.middleware.js";
+
+import { authorizeRole, authorizePermission } from "../middlewares/authorizeRole.middleware.js";
+
+import { roleRequestValidate } from "../validators/roleRequest.validator.js";
+
+const router = Router();
+
+router.post(
+  "/apply-roleRequest",
+  authentication,
+  validateRequest(roleRequestValidate.applyRoleRequestSchema),
+  roleRequestController.applyRoleRequest
+);
+
+router.post(
+  "/approve-role/:requestId",
+  authentication,
+  authorizeRole(process.env.ROLE_SUPER_ADMIN, process.env.ROLE_ADMIN),
+  authorizePermission("adminPanelAccess"),
+  validateRequest(roleRequestValidate.approveRoleSchema, "params"),
+  roleRequestController.approveRole
+);
+
+router.post(
+  "/reject-role/:requestId",
+  authentication,
+  authorizeRole(process.env.ROLE_SUPER_ADMIN, process.env.ROLE_ADMIN),
+  authorizePermission("adminPanelAccess"),
+  validateRequest(roleRequestValidate.rejectRoleSchema, "params"),
+  roleRequestController.rejectRole
+);
+
+router.post(
+  "/removeRequest-notification/:requestId",
+  authentication,
+  authorizeRole(process.env.ROLE_SUPER_ADMIN, process.env.ROLE_ADMIN),
+  authorizePermission("adminPanelAccess"),
+  validateRequest(
+    roleRequestValidate.removeRequestNotificationSchema,
+    "params"
+  ),
+  roleRequestController.removeRequestNotification
+);
+
+export default router;
