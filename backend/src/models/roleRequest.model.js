cb0aae3143a95274b95374752b913c09253a69ab
Sahil Yatam <sahilyatam9@gmail.com>
2025-10-30 15:53:08 +0530

    feat(roleRequest): define model schema for role request entity

---
+++ backend/src/models/roleRequest.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const roleRequestchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    requestedRole: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Role",
+        required: true,
+    },
+
+    status: {
+        type: String,
+        enums: ["Pending", "Approved", "Rejected"],
+        default: "Pending",
+    },
+
+    reviewedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+}, {timestamps: true});
+
+roleRequestchema.index({user: 1, requestedRole: 1, status: 1}, {unique: true, partialFilterExpression: {status: "Pending"}});
+
+export const RoleRequest = mongoose.model("RoleRequest",roleRequestchema);
