c40e34203d274570f79d793c39715ca4212c07ad
Sahil Yatam <sahilyatam9@gmail.com>
2025-10-30 17:17:32 +0530

    feat(user-repository): implement role population and assignment methods in user repository

--- backend/src/repositories/user.repository.js
+++ backend/src/repositories/user.repository.js
@@ -5,14 +5,28 @@ const findByEmail = (email) => {
     return User.findOne({email: userEmail}).lean();
 }
 const findById = (id) => User.findById(id).lean();
+
 const createUser = (data) => User.create(data);
+
 const updateById = (id, updates) => User.findByIdAndUpdate(id, updates, {new: true});
+
+const findByIdWithRole = (id) => 
+    User.findById(id)
+        .select("role")
+        .populate("role", "name key permissions")
+
 const findByResetToken = (token) => User.findOne({resetPasswordToken: token});
+
+const assignRoleToUser = (userId, roleId) =>
+    User.findByIdAndUpdate(userId, { role: roleId }, { new: true }).populate("role");
+
 
 export const userRepo = {
     findByEmail,
     findById,
+    findByIdWithRole,
     createUser,
     updateById,
-    findByResetToken
+    findByResetToken,
+    assignRoleToUser
 };
