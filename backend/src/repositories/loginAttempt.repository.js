4fa1276e378bb49619301a19afc7d154fe514a4a
Sahil Yatam <sahilyatam9@gmail.com>
2025-11-04 21:00:27 +0530

    refactor(loginAttempt-repo): normalize email case and add explicit returns for better consistency

--- backend/src/repositories/loginAttempt.repository.js
+++ backend/src/repositories/loginAttempt.repository.js
@@ -1,38 +1,47 @@
 import { LoginAttempt } from "../models/loginAttempt.model.js";
 
 
-const createAttempt = (data) => LoginAttempt.create(data);
+const createAttempt = (data) => {
+    return LoginAttempt.create(data)
+};
 
-const findByEmailAndIP = (email, ip) => LoginAttempt.findOne({ email, ip });
+const findByEmailAndIP = (email, ip) => {
+    const userEmail = email.trim().toLowerCase();
+    return  LoginAttempt.findOne({ email: userEmail, ip })
+};
 
 const incrementAttempt = async (email, ip) => {
+    const userEmail = email.trim().toLowerCase();
     return await LoginAttempt.findOneAndUpdate(
-        {email, ip},
+        {email: userEmail, ip},
         {$inc: {attempts: 1}, $set: { lastAttemptAt: new Date() }},
         {new: true, upsert: true} // upsert ensures a record exists
     );
 };
 
 const resetAttempts = async (email, ip) => {
+    const userEmail = email.trim().toLowerCase();
     return await LoginAttempt.findOneAndUpdate(
-        {email, ip},
+        {email: userEmail, ip},
         {attempts: 0, isBlocked: false, blockedUntil: null},
         {new: true}
     );
 };
 
 const blockTemporarily = async (email, ip) => {
+    const userEmail = email.trim().toLowerCase();
     const blockedUntil = new Date(Date.now() + 30 * 60 * 1000);
     return await LoginAttempt.findOneAndUpdate(
-        {email, ip},
+        {email: userEmail, ip},
         {isBlocked: true, blockedUntil},
         {new: true}
     );
 };
 
 const blockPermanently = async (email, ip) => {
+    const userEmail = email.trim().toLowerCase();
     return await LoginAttempt.findOneAndUpdate(
-        {email, ip},
+        {email: userEmail, ip},
         {permanentlyBlocked: true, isBlocked: true},
         {new: true}
     );
