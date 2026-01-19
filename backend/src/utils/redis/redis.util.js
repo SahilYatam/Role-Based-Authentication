c9e5c8d6e63e144867af8994ee38dfda8bb0c922
Sahil Yatam <sahilyatam9@gmail.com>
2026-01-19 18:42:10 +0530

    fix(redis): update SET TTL syntax for Upstash compatibility

--- backend/src/utils/redis/redis.util.js
+++ backend/src/utils/redis/redis.util.js
@@ -1,34 +1,36 @@
-import {redis} from "../../config/redis.config.js"
+import { redis } from "../../config/redis.config.js";
 
 const buildTokenKey = (userId, hashedToken) => {
     return `token:${userId}:${hashedToken}`;
-}
+};
 
-const cacheToken = async (userId, hashedToken, expiresAtMs) => {
+const cacheToken = async (userId, hashedToken, expiresAt) => {
     const ttlSeconds = Math.max(
         1,
-        Math.floor((expiresAtMs - Date.now()) / 1000)
-    )
+        Math.floor(expiresAt - Date.now() / 1000)
+    );
 
     const key = buildTokenKey(userId, hashedToken);
-    await redis.set(key, "1", "EX", ttlSeconds);
+
+    await redis.set(key, "1", {
+        ex: ttlSeconds
+    });
 
     return key;
-}
+};
 
-const removeToken = async(userId, hashedToken) => {
+const removeToken = async (userId, hashedToken) => {
     const key = buildTokenKey(userId, hashedToken);
     await redis.del(key);
-}
+};
 
 const exists = async (key) => {
     return redis.exists(key);
 };
-
 
 export const redisUtils = {
     buildTokenKey,
     cacheToken,
     removeToken,
     exists
-}
+};
