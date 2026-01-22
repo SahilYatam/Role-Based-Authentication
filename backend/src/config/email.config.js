96494ab0113c52a2cdae8f1853d1b38ce2d56cb1
Sahil Yatam <sahilyatam9@gmail.com>
2026-01-22 13:20:48 +0530

    update email.config file again

--- backend/src/config/email.config.js
+++ backend/src/config/email.config.js
@@ -1,16 +1,18 @@
-import Brevo from "@getbrevo/brevo";
+import * as brevo from "@getbrevo/brevo";
 
-if (!process.env.BREVO_API_KEY) {
-    throw new Error("BREVO_API_KEY is missing");
+const apiKey = process.env.BREVO_API_KEY;
+
+if (!apiKey) {
+  throw new Error("BREVO_API_KEY is not set");
 }
 
 console.log("🚨 BREVO_API_KEY VALUE:", process.env.BREVO_API_KEY);
 console.log("🚨 BREVO_API_KEY EXISTS:", !!process.env.BREVO_API_KEY);
+console.log("BREVO KEY PREFIX:", process.env.BREVO_API_KEY?.slice(0, 6));
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
-apiInstance.setApiKey(
-    Brevo.TransactionalEmailsApiApiKeys.apiKey,
-    process.env.BREVO_API_KEY
-);
+const apiInstance = new brevo.TransactionalEmailsApi();
+
+apiInstance.authentications["apiKey"].apiKey = apiKey;
 
 export { apiInstance };
+
